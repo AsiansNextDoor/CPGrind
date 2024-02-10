4aa6b3d569b18b8e8bd6b647315806fd58080044
AsiansNextDoor <yinzixuansz@gmail.com>
2024-02-10 14:59:50 -0800

    Upsolve Silver Winter 23-24 Mock 5
    Upsolve
    - C. Cow Mafia

--- SilverWinterMock2324/Mock5/C_CowMafia.cpp
+++ SilverWinterMock2324/Mock5/C_CowMafia.cpp
@@ -12,72 +12,40 @@ signed main() {
     IOS;
 
     int n; cin >> n;
-    map<int,int> child;
     vector<vector<int>> adj(n+1);
     for (int i = 1; i <= n; i++) {
         int m; cin >> m;
         for (int j = 0; j < m; j++) {
             int c; cin >> c;
-            child[c]++;
             adj[c].push_back(i);
         }
     }
 
+    int fans = n*(n+1)/2;
+    for (int root = 1; root <= n; root++) {
+        int nodes = 0;
+        vector<int> visited(n+1, 0);
 
-    int maxc = 0, root = 1;
-    for (int i = 1; i <= n; i++) {
-        if (maxc < child[i]) {
-            maxc = child[i];
-            root = i;
-        }
-    }
-
-    vector<int> visited(n+1, 0);
-    priority_queue<pair<int,int>> pq;
-    vector<int> parent(n+1);
-    parent[root] = 0;
-    
-    queue<pair<int,int>> bfs;
-    bfs.push({root, 1});
-    visited[root] = 1;
-    while (!bfs.empty()) {
-        auto data = bfs.front(); bfs.pop();
-        int node = data.f, layer = data.s;
-
-        bool leaf = true;
-        for (int i: adj[node]) {
-            if (!visited[i]) {
-                leaf = false;
-                visited[i] = true;
-                bfs.push({i, layer+1});
-                parent[i] = node;
+        int ans = 0;
+        queue<pair<int,int>> bfs;
+        bfs.push({root, 1});
+        visited[root] = 1;
+        while (!bfs.empty()) {
+            auto data = bfs.front(); bfs.pop();
+            int node = data.f, d = data.s;
+            for (int i: adj[node]) {
+                if (!visited[i]) {
+                    visited[i] = 1;
+                    bfs.push({i, d+1});
+                }
             }
+
+            ans += d;
+            nodes++;
         }
 
-        if (leaf) {
-            pq.push({layer, node});
-        }
+        if (nodes == n) fans = min(ans, fans);
     }
-    
-    visited.clear();
-    visited.resize(n+1, 0);
-    visited[0] = 1;
 
-    vector<int> wages(n+1, 1);
-
-    ll ans = 0;
-    
-    while (!pq.empty()) {
-        auto data = pq.top(); pq.pop();
-        int layer = data.f, node = data.s;
-        int p = parent[node];
-        wages[p] += wages[node];
-        if (!visited[p]) {
-            pq.push({layer-1, p});
-            visited[p] = 1;
-        }
-    }
-    for (int i = 1; i <= n; i++) ans += wages[i];
-
-    cout << ans << endl;
+    cout << fans << endl;
 }
