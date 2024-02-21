89042d1cc58489012d49b7c5c71df9b05868a960
AsiansNextDoor <yinzixuansz@gmail.com>
2024-02-21 12:14:00 -0800

    USACO Silver February 2024

---
+++ USACO/Silver/Feb2024/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Debug checklist
+- DELETE CODE PORTIONS ONE BY ONE AND RUN
+- Check bounds
+- Long long
+- Sample case
+- Edge cases
+*/
+
+typedef long long ll;
+#define f first
+#define s second
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, x1; cin >> n >> x1;
+        vector<vector<int>> targets(n, 3); // y1, x2, y2
+        for (int i = 0; i < n; i++) cin >> targets[i][0] >> targets[i][2] >> targets[i][1]
+    }
+}
