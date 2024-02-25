6938ee517d1cc9e64eb30b7eebe348831be2556c
AsiansNextDoor <yinzixuansz@gmail.com>
2024-02-25 09:55:32 -0800

    SOLVED BAKERY
    LETS GO!

--- USACO/Silver/Feb2023/A_Bakery.cpp
+++ USACO/Silver/Feb2023/A_Bakery.cpp
@@ -12,38 +12,41 @@ typedef long long ll;
 #define f first
 #define s second
 
-int n, tc, tm;
-vector<vector<int>> cows(100, vector<int>(3));
+ll n, tc, tm;
+vector<vector<ll>> cows(100, vector<ll>(3));
 
-bool checkc (int x, int m) {
-    for (int i = 0; i < n; i++) {
-        int a = cows[i][0], b = cows[i][1], c = cows[i][2];
+bool checkc (ll x, ll m) {
+    //cout << x << " xm " << m << endl;
+    for (ll i = 0; i < n; i++) {
+        ll a = cows[i][0], b = cows[i][1], c = cows[i][2];
         if (a < b) continue;
         
-        int tot = a*(tc-x) + b*(tm-(m-x));
+        ll tot = a*(tc-x) + b*(tm-(m-x));
+        //cout << tot << " " << c << endl;
         if (tot > c) return false;
     }
 
     return true;
 }
 
-bool checkm (int x, int m) {
-    for (int i = 0; i < n; i++) {
-        int a = cows[i][0], b = cows[i][1], c = cows[i][2];
+bool checkm (ll x, ll m) {
+    for (ll i = 0; i < n; i++) {
+        ll a = cows[i][0], b = cows[i][1], c = cows[i][2];
         if (a >= b) continue;
         
-        int tot = a*(tc-(m-x)) + b*(tm-x);
+        ll tot = a*(tc-(m-x)) + b*(tm-x);
         if (tot > c) return false;
     }
 
     return true;
 }
 
-bool check (int x) {
-    int c, m;
-    int l = 0, r = min(tc-1, x);
+bool check (ll x) {
+    ll c = tc-1, m = tm-1;
+    ll l = 0, r = min(tc-1, x);
     while (l <= r) {
-        int mid = (l + r) / 2;
+        ll mid = (l + r) / 2;
+        //cout << "mid: " << mid << endl;
         if (checkc(mid, x)) {
             c = mid;
             r = mid - 1;
@@ -51,12 +54,13 @@ bool check (int x) {
         else {
             l = mid + 1;
         }
+        //cout << endl << endl;
     }
 
     
     l = 0, r = min(tm-1, x);
     while (l <= r) {
-        int mid = (l + r) / 2;
+        ll mid = (l + r) / 2;
         if (checkm(mid, x)) {
             m = mid;
             r = mid - 1;
@@ -66,7 +70,7 @@ bool check (int x) {
         }
     }
 
-    cout << x << " " << c << " " << m << endl;
+    //cout << x << " " << c << " " << m << endl;
     if (c + m > x) return false;
     return true;
 }
@@ -75,12 +79,12 @@ signed main() {
     IOS;
     ll t; cin >> t;
     while (t--) {
-        int n, tc, tm; cin >> n >> tc >> tm;
-        for (int i = 0; i < n; i++) cin >> cows[i][0] >> cows[i][1] >> cows[i][2];
+        cin >> n >> tc >> tm;
+        for (ll i = 0; i < n; i++) cin >> cows[i][0] >> cows[i][1] >> cows[i][2];
 
-        int l = 0, r = tc+tm-2, ans = 0;
+        ll l = 0, r = tc+tm-2, ans = 0;
         while (l <= r) {
-            int mid = (l + r) / 2;
+            ll mid = (l + r) / 2;
             if (check(mid)) {
                 ans = mid;
                 r = mid - 1;
