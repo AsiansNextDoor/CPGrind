d9de3e3ab828e21335d2e3205b3be10957e8a902
AsiansNextDoor <yinzixuansz@gmail.com>
2024-02-24 18:33:25 -0800

    Silver Winter 23-24 Mock 6
    Solved
    - A. Are the Cups Enough
    - B. Barn Layouts

--- USACO/Silver/Feb2023/A_Bakery.cpp
+++ USACO/Silver/Feb2023/A_Bakery.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
 using namespace std;
 
 typedef long long ll;
@@ -8,70 +12,62 @@ typedef long long ll;
 #define f first
 #define s second
 
-ll n, timec, timem;
-vector<vector<ll>> cows(100, vector<ll>(3)); // cookies, muffins, wait
+int n, tc, tm;
+vector<vector<int>> cows(100, vector<int>(3));
 
-bool check(ll m) { //money
-    ll maxl=0, minr=1e9; //range of possible changes to cookie efficiency
-
-    for (ll i = 0; i < n; i++) {
-        ll crate = cows[i][0];
-        ll mrate = cows[i][1];
-        ll target = cows[i][2];
-
-        ll tot, ccount, mcount, l, r;
-        if (crate > mrate) {
-            if (m > timec-1) {
-                ccount = timec-1;
-                mcount = min(m - ccount, timem-1);
-            }
-            else {
-                ccount = m;
-                mcount = 0;
-            }
-            tot = crate*(timec - ccount) + mrate*(timem - mcount);
-            if (tot > target) return false;
-            
-            ll diffrate = crate - mrate;
-            ll diff;
-            if (diffrate == 0) {
-                diff = 1e9;
-            }
-            l = max(ccount-diff, 0LL);
-            r = ccount;
-        }
-        else {
-            if (m > timem-1) {
-                mcount = timem-1;
-                ccount = min(m - mcount, timec-1);
-            }
-            else {
-                mcount = m;
-                ccount = 0;
-            }
-            tot = crate*(timec - ccount) + mrate*(timem - mcount);
-            if (tot > target) return false;
-            
-            ll diffrate = mrate - crate;
-            ll diff;
-            if (diffrate == 0) {
-                diff = 1e9;
-            }
-            else diff = (target - tot) / diffrate;
-            l = ccount;
-            r = min(ccount+diff, timec-1);
-        }
-
-        cout << "l: " << l << endl;
-        cout << "r: " << r << endl;
-        cout << endl;
-
-        maxl = max(l, maxl);
-        minr = min(r, minr);
+bool checkc (int x, int m) {
+    for (int i = 0; i < n; i++) {
+        int a = cows[i][0], b = cows[i][1], c = cows[i][2];
+        if (a < b) continue;
+        
+        int tot = a*(tc-x) + b*(tm-(m-x));
+        if (tot > c) return false;
     }
 
-    if (maxl > minr) return false;
-    cout << maxl << " " << minr << endl;
+    return true;
+}
+
+bool checkm (int x, int m) {
+    for (int i = 0; i < n; i++) {
+        int a = cows[i][0], b = cows[i][1], c = cows[i][2];
+        if (a >= b) continue;
+        
+        int tot = a*(tc-(m-x)) + b*(tm-x);
+        if (tot > c) return false;
+    }
+
+    return true;
+}
+
+bool check (int x) {
+    int c, m;
+    int l = 0, r = min(tc-1, x);
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (checkc(mid, x)) {
+            c = mid;
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    
+    l = 0, r = min(tm-1, x);
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (checkm(mid, x)) {
+            m = mid;
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    cout << x << " " << c << " " << m << endl;
+    if (c + m > x) return false;
     return true;
 }
 
@@ -79,12 +75,12 @@ signed main() {
     IOS;
     ll t; cin >> t;
     while (t--) {
-        cin >> n >> timec >> timem;
-        for (ll i = 0; i < n; i++) cin >> cows[i][0] >> cows[i][1] >> cows[i][2];
+        int n, tc, tm; cin >> n >> tc >> tm;
+        for (int i = 0; i < n; i++) cin >> cows[i][0] >> cows[i][1] >> cows[i][2];
 
-        ll l = 0, r = timec+timem-2, ans = 5;
+        int l = 0, r = tc+tm-2, ans = 0;
         while (l <= r) {
-            ll mid = (l + r) / 2;
+            int mid = (l + r) / 2;
             if (check(mid)) {
                 ans = mid;
                 r = mid - 1;
