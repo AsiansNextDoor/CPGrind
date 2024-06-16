c6bd67b6f64a9e64a70b2c8b1588efbc2791819d
AsiansNextDoor <yinzixuansz@gmail.com>
2024-06-15 23:06:08 -0700

    Digit DP and Magic Numbers

---
+++ X_Camp/CS401/Week6/MagicNumbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define f first
+#define s second
+
+signed main() {
+    IOS;
+
+    int m, d; cin >> m >> d;
+    string a, b; cin >> a >> b;
+}
