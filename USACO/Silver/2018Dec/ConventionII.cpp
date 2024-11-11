5a15da5b5a1375a8639bf2d70b71be60d84c6d12
AsiansNextDoor <yinzixuansz@gmail.com>
2024-11-10 18:24:31 -0800

    Convention II

--- USACO/Silver/2018Dec/ConventionII.cpp
+++ USACO/Silver/2018Dec/ConventionII.cpp
@@ -1,21 +1,30 @@
-//#pragma GCC optimize ("trapv")
+// #pragma GCC optimize ("trapv")
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 #define endl "\n"
 
 int INF, MOD;
 
-signed main() {
+signed main()
+{
     IOS;
 
-    int n; cin >> n;
+    freopen("convention2.in", "r", stdin);
+    freopen("convention2.out", "w", stdout);
+
+    int n;
+    cin >> n;
     vector<vector<int>> a(n, vector<int>(3));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i][0] >> a[i][2];
         a[i][1] = i;
     }
@@ -24,19 +33,23 @@ signed main() {
 
     int atime = 0, ans = 0;
     set<vector<int>> s;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int t = a[i][0], dur = a[i][2], rank = a[i][1];
-        while (!s.empty() && atime > t) {
+        while (!s.empty() && atime < t)
+        {
             auto it = s.begin();
+            if (atime < (*it)[1])
+                atime = (*it)[1];
             ans = max(atime - (*it)[1], ans);
             atime += (*it)[2];
             s.erase(it);
         }
-        
+
         s.insert({rank, t, dur});
-        if (atime < t) atime = t;
     }
-    while (!s.empty()) {
+    while (!s.empty())
+    {
         auto it = s.begin();
         ans = max(atime - (*it)[1], ans);
         atime += (*it)[2];
--- test.cpp
+++ test.cpp
@@ -1,6 +1,40 @@
+// #pragma GCC optimize ("trapv")
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << "helloworld" << endl;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl "\n"
+
+int INF, MOD;
+
+signed main()
+{
+    IOS;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        vector<string> calico = {"o", "co", "ico", "lico", "alico", "calico"};
+        for (int i = 0; i < 6; i++)
+        {
+            if (s.size() <= i)
+                break;
+            if (s.substr(0, i + 1) == calico[i])
+            {
+                s = "CALICO" + s.substr(i + 1, s.size() - (i + 1));
+                break;
+            }
+        }
+
+        cout << s << endl;
+    }
 }
